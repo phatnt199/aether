435b418a7081a4838725b44551fd5d0864abd650
Phat Nguyen <tanphat199@gmail.com>
2025-11-02 15:43:32 +0700

    diable eslint @typescript-eslint/unified-signatures

--- packages/node-infra/eslint.config.mjs
+++ packages/node-infra/eslint.config.mjs
@@ -9,6 +9,7 @@ const configs = [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-dupe-class-members': 'off',
       '@typescript-eslint/await-thenable': 'off',
+      '@typescript-eslint/unified-signatures': 'off',
     },
   },
 ];
