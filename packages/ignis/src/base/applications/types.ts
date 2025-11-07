9075c61dd385d6adc0c53b7807b2a7b8ae61132c
Phat Nguyen <tanphat199@gmail.com>
2025-11-07 12:23:20 +0700

    add extra application config

--- packages/ignis/src/base/applications/types.ts
+++ packages/ignis/src/base/applications/types.ts
@@ -1,13 +1,59 @@
+import type { Context } from 'hono';
+
+// ------------------------------------------------------------------------------
+// CORS Options
+// ------------------------------------------------------------------------------
+export interface ICORSOptions {
+  origin:
+    | string
+    | string[]
+    | ((
+        origin: string,
+        c: Context,
+      ) => Promise<string | undefined | null> | string | undefined | null);
+  allowMethods?: string[] | ((origin: string, c: Context) => Promise<string[]> | string[]);
+  allowHeaders?: string[];
+  maxAge?: number;
+  credentials?: boolean;
+  exposeHeaders?: string[];
+}
+
+// ------------------------------------------------------------------------------
+// CSRF Options
+// ------------------------------------------------------------------------------
+type IsAllowedOriginHandler = (origin: string, context: Context) => boolean;
+declare const secFetchSiteValues: readonly ['same-origin', 'same-site', 'none', 'cross-site'];
+type SecFetchSite = (typeof secFetchSiteValues)[number];
+type IsAllowedSecFetchSiteHandler = (secFetchSite: SecFetchSite, context: Context) => boolean;
+export type TCSRFOptions = {
+  origin?: string | string[] | IsAllowedOriginHandler;
+  secFetchSite?: SecFetchSite | SecFetchSite[] | IsAllowedSecFetchSiteHandler;
+};
+
+// ------------------------------------------------------------------------------
+// Body Limit Options
+// ------------------------------------------------------------------------------
+export interface TBodyLimitOptions {
+  maxSize: number;
+  onError?: (c: Context) => Response | Promise<Response>;
+}
+
+// ------------------------------------------------------------------------------
+// Application
+// ------------------------------------------------------------------------------
 export interface IApplicationConfig {
-  port?: number;
-  host?: string;
   basePath?: string;
-  cors?: boolean;
+  compress?: boolean;
+
+  cors?: ICORSOptions;
+  bodyLimit?: TBodyLimitOptions;
+
   autoLoad: {
     dirs: Record<
       string, // folder name | namespace
       string // folder path from basePath
     >;
   };
+
   [key: string]: any;
 }
